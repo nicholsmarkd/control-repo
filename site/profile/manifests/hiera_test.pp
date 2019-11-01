6c19d7981e063a31ea2760cdb67e7da7ac298504
mark.nichols <markn.psalm37@gmail.com>
2019-10-31 21:21:32 -0400

    update

--- site/profile/manifests/hiera_test.pp
+++ site/profile/manifests/hiera_test.pp
@@ -15,5 +15,11 @@ class profile::hiera_test (
                |END
     owner   => root,
     mode    => '0644',
-  }
+  },
+  file { '/tmp/newfile.txt':
+    ensure => file,
+    content => newfile,
+    owner => root,
+    mode => '0644',
+  },
 }
